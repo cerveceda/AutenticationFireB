46ee3e2933924e00a925d19d2f364f3bfcf5d782
Erian <54561501+Seagull29@users.noreply.github.com>
2021-06-29 20:52:30 -0500

    interfaGra

--- app/src/main/java/com/example/autenticationfireb/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/autenticationfireb/ui/home/HomeFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,19 +20,25 @@ import com.example.autenticationfireb.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private LinearLayout btnPlay, btnStop, btnRecord;
+    private WebView webView;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        this.btnPlay = root.findViewById(R.id.btnPlay);
+        this.btnStop = root.findViewById(R.id.btnStop);
+        this.btnRecord = root.findViewById(R.id.btnRecord);
+
+        this.webView = root.findViewById(R.id.wvWeb);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.youtube.com");
+
         return root;
     }
 }
