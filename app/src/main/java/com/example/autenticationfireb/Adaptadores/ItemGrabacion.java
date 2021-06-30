54f7227ee6846fd6fb7da7a2581728cf9cee7142
Shannon <shannonelison56@gmail.com>
2021-06-30 16:39:51 -0500

    casi

--- app/src/main/java/com/example/autenticationfireb/Adaptadores/ItemGrabacion.java
+++ app/src/main/java/com/example/autenticationfireb/Adaptadores/ItemGrabacion.java
@@ -11,10 +11,10 @@ public class ItemGrabacion {
     //contenido del arreglo
     static {
         addItem(new ItemGrabacion.itemGab ("1","Grabacion", R.drawable.musica,"Grabacion0.m4a"));
-        addItem(new ItemGrabacion.itemGab("2","Grabacion1",R.drawable.musica,"/storage/emulated/Grabacion1.m4a"));
-        addItem(new ItemGrabacion.itemGab("3","Grabacion2", R.drawable.musica,"/storage/emulated/Grabacion2.m4a"));
-        addItem(new ItemGrabacion.itemGab("4","Grabacion3", R.drawable.musica,"/storage/emulated/Grabacion3.m4a"));
-        addItem(new ItemGrabacion.itemGab("5","Grabacion4", R.drawable.musica,"/storage/emulated/Grabacion4.m4a"));
+        addItem(new ItemGrabacion.itemGab("2","Grabacion1",R.drawable.musica,"Grabacion1.m4a"));
+        addItem(new ItemGrabacion.itemGab("3","Grabacion2", R.drawable.musica,"Grabacion2.m4a"));
+        addItem(new ItemGrabacion.itemGab("4","Grabacion3", R.drawable.musica,"Grabacion3.m4a"));
+        addItem(new ItemGrabacion.itemGab("5","Grabacion4", R.drawable.musica,"Grabacion4.m4a"));
     }
     //metodo que contruye el arreglo , metodo principal
     static void addItem(ItemGrabacion.itemGab item){
--- app/src/main/java/com/example/autenticationfireb/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/autenticationfireb/ui/slideshow/SlideshowFragment.java
@@ -1,8 +1,14 @@
 package com.example.autenticationfireb.ui.slideshow;
 
+import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.database.Cursor;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -15,6 +21,10 @@ import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -30,13 +40,51 @@ import com.example.autenticationfireb.R;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+
 
 public class SlideshowFragment extends Fragment {
 
     private SlideshowViewModel slideshowViewModel;
     AdapterGrabacion adapter;
     ListView lista;
+    String outputFile=null;
+    MediaPlayer mediaPlayer = new MediaPlayer();
+    private final int PICKER=1;
+    private int state =1;
+    private final int Playing=1;
+    private final int Pausing=2;
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent data = result.getData();
+                        String FilePath =data.getData().getPath();
+                        try{
+                            mediaPlayer. setDataSource (FilePath) ;
+                            mediaPlayer.prepare();
+                        }catch (IOException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+
+    private void PickFile (){
+    Intent intent= new Intent (Intent.ACTION_GET_CONTENT);
+intent.setType ("Eile/*");
+        intent.addCategory (Intent.CATEGORY_OPENABLE);
+            try{
+                someActivityResultLauncher.launch(Intent.createChooser(intent, "Instale un adninistradorde archivos."));
+            }catch (android.content.ActivityNotFoundException ex){}}
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         slideshowViewModel =
@@ -53,9 +101,12 @@ public class SlideshowFragment extends Fragment {
                 // La posición donde se hace clic en el elemento de lista se obtiene de la
                 // la posición de parámetro de la vista de lista de Android
                 ItemGrabacion.itemGab item = (ItemGrabacion.itemGab) parent.getItemAtPosition(position);
-                Intent intent = new Intent(getActivity().getBaseContext(), MenuDrawerActivity.class);
+               Intent intent = new Intent(getActivity().getBaseContext(), MenuDrawerActivity.class);
                 intent.putExtra("id", item.getId());
-               
+                PickFile();
+                mediaPlayer.start();
+
+            //startActivity(intent);
 
             }
         });
